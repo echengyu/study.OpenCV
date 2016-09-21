4c8176213a7fc8d45a53d466fe419166b9f1b03f
Cheng-Yu <e88885@gmail.com>
2016-09-21 12:51:40 +0800

    更新

--- src/edu/neu/mhealth/debug/CameraActivity.java
+++ src/edu/neu/mhealth/debug/CameraActivity.java
@@ -37,16 +37,10 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 
 	/*Basic Variables*/
 	private final String TAG = Global.APP_LOG_TAG;
-	private final int ACCUMULATOR_THRESHOLD = 50;
-	private final int MINLINELENGTH = 100;
-	private final int MAXLINEGAP = 80;
-	private final int HYSTERESIS_THRESHOLD1 = 50;
-	private final int HYSTERESIS_THRESHOLD2 = 400;
 	private final int HOUGH_LINE_COUNT = 20;
 	
 	private List<Point> points1 = new ArrayList<Point>();
 	private List<Point> points2 = new ArrayList<Point>();
-	private Rect roi = new Rect(300, 50, 50, 10);
 	
 	/*OpenCv Variables*/
 	private Mat mRgba;
@@ -106,8 +100,6 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 		}
 	}
 
-	
-	
 	/*
 	 *   Opencv Callbacks
 	 *   
@@ -128,88 +120,48 @@ public class CameraActivity extends Activity implements CvCameraViewListener2 {
 		mRgba = inputFrame.rgba();
 		mGray = inputFrame.gray();
 		Mat cannyMat = new Mat();
-//		mGray.submat(1, mGray.rows()-1, 1, mGray.cols()-1).copyTo(cannyMat);
 		
-//		mRgba = new Mat(inputFrame.rgba(), roi);
-		
-//		Mat matCvtColor = new Mat();		// 色彩轉換
-//		Mat matGaussianBlur = new Mat();	// 高斯濾波器
-//		Mat matCanny = new Mat();			// 邊緣偵測
-//		Mat matErode = new Mat();			// 蝕刻
-//		Mat matdilate = new Mat();			// 色彩轉換
-		
-//		Imgproc.cvtColor(mRgba, matCvtColor, Imgproc.COLOR_RGBA2GRAY, 4);
-//		Imgproc.GaussianBlur(matCvtColor, matGaussianBlur, new Size(7, 7), 2);
-//		Imgproc.Canny(matGaussianBlur, matCanny, 200, 250);
-//		Imgproc.erode(matCanny, matErode, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(4,4)));
-//      Imgproc.dilate(matErode, matdilate, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(4, 4)));
-		
+		// 二值化
 		Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGBA2GRAY, 0);
+		
+		// 高斯濾波器
 		Imgproc.GaussianBlur(mRgba, mRgba, new Size(3, 3), 6);
-		Imgproc.Canny(mRgba, mRgba, 220, 180);		
+		
+		// 邊緣偵測
+		Imgproc.Canny(mRgba, mRgba, 220, 180);	
+		
+		// 蝕刻
 		Imgproc.erode(mRgba, mRgba, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, 1)));
+		
+		// 膨脹
         Imgproc.dilate(mRgba, mRgba, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(4, 4)));
-
+        
+        // 霍夫找線
         Imgproc.HoughLinesP(mRgba, lines, 1, Math.PI/180, 180, 80, 0);		
-		
-//		Imgproc.cvtColor(mRgba, mGray,Imgproc.COLOR_RGBA2GRAY,4);
-		
-//		Imgproc.GaussianBlur(mGray, mGray, new Size(7, 7), 2);
-		
-//		Imgproc.Canny(mGray, cannyMat, HYSTERESIS_THRESHOLD1, HYSTERESIS_THRESHOLD2, 3, false);
-		
-//		Imgproc.HoughLinesP(cannyMat, lines, 1, Math.PI/180, ACCUMULATOR_THRESHOLD, MINLINELENGTH, MAXLINEGAP);
-//		Imgproc.HoughLinesP(cannyMat, lines, 1, Math.PI/180, 180, 100, 180);
 
-		for (int x = 0; x < lines.cols() && x < HOUGH_LINE_COUNT; x++) {
-	          double[] vec = lines.get(0, x);
-	        
-	          if(vec!=null) {
-	        	  double x1 = vec[0], 
-	        			  y1 = vec[1],
-	        			  x2 = vec[2],
-	        			  y2 = vec[3];
-	        	  
-	        	  if(x%2 == 1){
-	        		  points1.add(new Point(x1, y1));
-		        	  
-	        	  }else{
-	        		  points2.add(new Point(x2, y2));
-	        	  }
-	        	  
-//	        	  points1.add(new Point(x1, y1));
-//	        	  points2.add(new Point(x2, y2));
-	        	  
-//	        	  Log.e("lines.cols()", String.valueOf(lines.cols()));
-//	      		  Log.e("vec", String.valueOf(x1) + ">  " + String.valueOf(y1) + ">  " 
-//	      				  + String.valueOf(x1) + ">  " + String.valueOf(y2));
-	      		
-	        	  Point start = new Point(x1, y1);
-	        	  Point end = new Point(x2, y2);
-//	        	  Point end = new Point(10, 10);
-	      		  
-//	        	  Point start = new Point(10, 10);
-//	        	  Point end = new Point(200, 200);
-	        	  
-//	        	  Core.line(mRgba, start, end, new Scalar(255, 127, 63, 255), 20);
-	        	  
-//	        	  Core.rectangle(mRgba, start, end, new Scalar(255, 127, 63, 255), 20);
-	        	  
-//	        	  Core.rectangle(mRgba, points1.get(0),  points2.get(0), new Scalar(255, 127, 63, 255), 10);
-	        	 
-	          }
-	    }
+        for (int x = 0; x < lines.cols() && x < HOUGH_LINE_COUNT; x++) {
+        	double[] vec = lines.get(0, x);
+
+        	if(vec!=null) {
+        		double x1 = vec[0],
+        		       y1 = vec[1],
+        		       x2 = vec[2],
+        		       y2 = vec[3];
+
+        		if(x%2 == 1) {
+        			points1.add(new Point(x1, y1));
+        		} else {
+        			points2.add(new Point(x2, y2));
+        		}
+        	}
+        }
 		
 		Log.e("lines.cols()", String.valueOf(lines.cols()));
 		Log.e("points1", points1.toString());
 		Log.e("points2", points2.toString());		
 		
-		if(points1.size() != 0 && points2.size() != 0){
-//			double l = Math.abs(points1.get(0).x - points2.get(0).x);
-			
+		if(points1.size() != 0 && points2.size() != 0){			
 			if(Math.abs(points1.get(0).x - points2.get(0).x) > (points2.get(0).x / 5)){
-			
-//				Core.rectangle(mRgba, points1.get(0),  points2.get(0), new Scalar(255, 127, 63, 255), 10);
 				Core.rectangle(inputFrame.rgba(), points1.get(0),  points2.get(0), new Scalar(255, 127, 63, 255), 10);
 				
 				
